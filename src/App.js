99d49fdcad8e05eefec01c00820e2f2468883f6d
sachin320 <sachinvermavs786@gmail.com>
2021-10-04 00:42:50 +0530

    Component Added Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/nav';
+import Home from './components/home';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const HomePage = () =>{
+    return(
+       <>
+       <div className='Home'>
+           <h1>"Don't focus on having a great blog. Focus on producing a blog that's great for your readers."</h1>
+           <span>Brian Clark</span>
+           <button className="nav-btn">Publish Your blog</button>
+       </div>
+       </>
+    )
+}
+
+export default HomePage;
