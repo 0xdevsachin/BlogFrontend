2e214f06a8159986bef6ff76eea40ae173adae27
sachin320 <sachinvermavs786@gmail.com>
2021-10-03 23:31:41 +0530

    NavBar Updated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,30 @@
 import '../App.css';
+
 const navbar = () =>{
+    const Hello =  () =>{
+        const navlink = document.querySelector('.nav-links');
+        navlink.classList.toggle('nav-active');
+    }
     return(
+        <>
         <nav>
-            <p>logo</p>
+            <div className='nav-logo'>
+                <h4>Logo</h4>
+            </div>
             <ul className="nav-links">
                 <li>Home</li>
                 <li>About</li>
-                <li>read</li>
-                <li>contact</li>
+                <li>Work</li>
+                <li>Contact</li>
+                <li><button className="nav-btn">Sign In</button></li>
             </ul>
-           <button className="nav-btn">Sign in</button>
+           <div className='nav-menu' onClick={Hello}>
+               <div className='line1'></div>
+               <div className='line2'></div>
+               <div className='line3'></div>
+           </div>
         </nav>
+        </>
     );
 }
 
