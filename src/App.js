8f59520141120b6c307c964ee2be22a98953a4ab
sachin320 <sachinvermavs786@gmail.com>
2021-10-04 10:34:15 +0530

    Component Added Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/nav';
 import Home from './components/home';
+import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Footer = () =>{
+    return(
+        <>
+        <div className='Footer-Area'>
+        <div className='Footer'>
+            <div><h3>Logo</h3></div>
+            <div>
+                <ul className='Social-Footer-li'>
+                    <li>icon</li>
+                    <li>icon</li>
+                    <li>icon</li>
+                    <li>icon</li>
+                    <li>icon</li>
+                </ul>
+            </div>
+        </div>
+        <div className='Footer-hr'>
+        <hr  />
+        <p style={{textAlign:'center',color:'white',margin:'12px 0'}}>&#169;2021. All rights Reserved</p>
+        <p style={{textAlign:'center',color:'white',margin:'12px 0'}}>Created by <a href="https://github.com/0xdevsachin" style={{color:'white'}} target='_blank'>Dev Sachin</a></p>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer;
