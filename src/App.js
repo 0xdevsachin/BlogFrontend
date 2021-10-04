7ff8b817ca22ff39dceb3e6000fd7675930fec12
sachin320 <sachinvermavs786@gmail.com>
2021-10-04 15:24:19 +0530

    component Added About

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/nav';
 import Home from './components/home';
 import Footer from './components/footer';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import About from './components/about';
 function App() {
   return (
     <Router>
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/about" component={Home} />
+          <Route path="/about" component={About} />
           <Route path="/work" component={Home} />
           <Route path="/contact" component={Home} />
           <Route path="/signin" component={Home} />
---
+++ src/components/about.jsx
@@ -0,0 +1,52 @@
+import '../App.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+const About = ()=>{
+    return(
+        <>
+        <div className="About">
+        <div className='About-header'>
+            <h1>About Blogger</h1>
+        </div>
+        <div className="About-Content">
+            <div>
+                <p>Online Publishing Platform. Where you can Read about job opportunities, career advice, expert's insights, new features and products.
+                We got you covered with fact-based articles Gwoth and career guides. 
+                Read Blog</p>
+            </div>
+            <div className="cardDiv">
+                <div className='card'>
+                    <p>Technology</p>
+                    <span className='card-content'>
+                        Read about Latest Technology released in market.
+                    </span>
+                </div>
+                <div className='card'>
+                    <p>Jobs</p>
+                    <span className='card-content'>
+                        Get Updated with Latest jobs.
+                    </span>
+                </div>
+                <div className='card'>
+                    <p>Carrers</p>
+                    <span className='card-content'>
+                        Read About Some Carrer Tips
+                    </span>
+                </div>
+                <div className='card'>
+                    <p>Write</p>
+                    <span className='card-content'>
+                        Write and share your blogs with the world.
+                    </span>
+                </div>
+            </div>   
+        </div>
+        <div>
+           <Link to="/about"> <button className="About-btn">Write blog</button></Link>
+        </div>
+        </div>
+        </>
+    );
+}
+
+export default About;
