e07e01e98fa96549974bdf861d7c0e760472610e
sachin320 <sachinvermavs786@gmail.com>
2021-10-04 11:04:49 +0530

    Router Added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import Navbar from './components/nav';
 import Home from './components/home';
 import Footer from './components/footer';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
