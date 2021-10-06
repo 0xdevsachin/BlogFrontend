7a0bf3ec3c44c4ec16ba8736c57c26d4a722232d
sachin320 <sachinvermavs786@gmail.com>
2021-10-07 00:23:22 +0530

    Navbar Function Updated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,6 +5,10 @@ const navbar = () =>{
     const NavActive =  () =>{
         const navlink = document.querySelector('.nav-links');
         navlink.classList.toggle('nav-active');
+        const lists = document.querySelector('ul');
+        lists.addEventListener('click', ()=>{
+            navlink.classList.remove('nav-active');
+        })
     }
     return(
         <>
