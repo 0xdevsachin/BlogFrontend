bef118a2f2bcf5e808abc8e3df02c02a78f335e7
sachin320 <sachinvermavs786@gmail.com>
2021-10-08 16:18:27 +0530

    Backend Connected

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import '../App.css';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 const Signup = () =>{
+    const Register = async (e) =>{
+        e.preventDefault();
+        axios.post('/api').then((data)=>{
+            console.log(data.data);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
     return(
         <>
         <div className="Signin">
             <div className="Sign-in-form">
-                <form action="" onSubmit={(e)=>{
-                    e.preventDefault();
-                }} className="input-form">
+                <form  onSubmit={Register} method="POST" className="input-form">
                     <h1>Sign Up</h1>
                     <hr style={{width:'90%', margin:'auto', marginTop:'20px', marginBottom:'50px'}} />
                     <label htmlFor=""><i className="fa fa-user icon"> </i></label>
@@ -23,8 +30,8 @@ const Signup = () =>{
                     <label htmlFor=""><i className="fa fa-key icon"> </i></label>
                     <input type="password" placeholder="Confirm Password" />
                     <br />
-                    <div>
-                        <Link to="/signin"><p className="form-link">Already have an account ?</p></Link>
+                    <div className="form-link">
+                        <Link to="/signin"><p className="form-link2">Already have an account</p></Link>
                     </div>
                     <button className="Sign-btn">Sign Up</button>
                 </form>
