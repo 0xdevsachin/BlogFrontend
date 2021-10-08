b73303ae97ef45820339c18430f69dc186dda2a1
sachin320 <sachinvermavs786@gmail.com>
2021-10-09 00:07:37 +0530

    Sign up Working

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -12,12 +12,18 @@ const Signup = () =>{
     } 
     const Register = async (e) =>{
         e.preventDefault();
-        console.log(FormData);
-        axios.post('/api/auth/signup', FormData).then().catch((err)=>{
-            console.log(err);
-        })
-        alert('Account Registered successfully');
-        history.push("/signin");
+        if(FormData.cpassword === FormData.password){
+            console.log(FormData);
+            axios.post('/api/auth/signup', FormData).then((data)=>{
+                console.log(data);
+                alert(data.data.msg);
+                if(data.data.redirect){
+                    history.push("/signin")
+                }
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
     }
     return(
         <>
@@ -27,16 +33,16 @@ const Signup = () =>{
                     <h1>Sign Up</h1>
                     <hr style={{width:'90%', margin:'auto', marginTop:'20px', marginBottom:'50px'}} />
                     <label htmlFor=""><i className="fa fa-user icon"> </i></label>
-                    <input type="text" placeholder="Username" className="inputs" onChange={(e)=>{FormData.username = e.target.value;}} />
+                    <input type="text" placeholder="Username" required className="inputs" onChange={(e)=>{FormData.username = e.target.value;}} />
                     <br />
                     <label htmlFor=""><i className="fa fa-envelope icon"> </i></label>
-                    <input type="text" placeholder="Email" className="inputs" onChange={(e)=>{FormData.email = e.target.value;}} />
+                    <input type="email" placeholder="Email" required className="inputs" onChange={(e)=>{FormData.email = e.target.value;}} />
                     <br />
                     <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" placeholder="Password" className="inputs" onChange={(e)=>{FormData.password = e.target.value;}} />
+                    <input type="password" placeholder="Password" required className="inputs" onChange={(e)=>{FormData.password = e.target.value;}} />
                     <br />
                     <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" placeholder="Confirm Password" className="inputs" onChange={(e)=>{FormData.cpassword = e.target.value;}} />
+                    <input type="password" placeholder="Confirm Password" required className="inputs" onChange={(e)=>{FormData.cpassword = e.target.value;}} />
                     <br />
                     <div className="form-link">
                         <Link to="/signin"><p className="form-link2">Already have an account</p></Link>
