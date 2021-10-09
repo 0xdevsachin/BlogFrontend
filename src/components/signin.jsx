3fd274050ff6024ac1b0e224844a25efb19b1bdd
sachin320 <sachinvermavs786@gmail.com>
2021-10-09 11:02:11 +0530

    sign in complete

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import '../App.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import axios from 'axios';
 const Signin = () =>{
+    const history = useHistory()
+    var FormData = {
+        username:"",
+        password:"",
+    }
+    const userSignin = (e) =>{
+        e.preventDefault();
+        if(FormData.username !== '' && FormData.password !== '' ){
+            axios.post("/api/auth/signin", FormData).then((data)=>{
+                console.log(data);
+                alert(data.data.msg)
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
+        else{
+            alert("Something Went Wrong :(")
+            history.push('/signin')
+        }
+    }
     return(
         <>
         <div className="Signin">
             <div className="Sign-in-form">
-                <form action="" onSubmit={(e)=>{
-                    e.preventDefault();
-                }} className="input-form">
+                <form action="" onSubmit={userSignin} className="input-form">
                     <h1>Sign In</h1>
                     <hr style={{width:'90%', margin:'auto', marginTop:'20px', marginBottom:'50px'}} />
                     <label htmlFor=""><i className="fa fa-user icon"> </i></label>
-                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Email" required onChange={(e) =>{FormData.username = e.target.value}} />
                     <br />
                     <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" onChange={(e) =>{FormData.password = e.target.value}} required />
                     <br />
                     <div className="form-link">
                         <Link to="/signup"><p className="form-link2">Create new account</p></Link>
