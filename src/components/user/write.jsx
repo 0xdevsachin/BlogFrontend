36d2f5aa6c8632f7c90c2ef0e0946a38371cf99a
sachin320 <sachinvermavs786@gmail.com>
2021-10-23 15:00:18 +0530

    Minor Update

--- src/components/user/write.jsx
+++ src/components/user/write.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../App.css';
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from 'axios';
 const Write = () =>{
     const history = useHistory();
