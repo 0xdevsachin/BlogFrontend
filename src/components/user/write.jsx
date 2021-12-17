8807cf9797b1886b238768230aa60f037fb42053
sachin320 <sachinvermavs786@gmail.com>
2021-12-17 17:46:18 +0530

    Write blog updated

--- src/components/user/write.jsx
+++ src/components/user/write.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import '../../App.css';
-// import axios from 'axios';
+import axios from 'axios';
+import userStateContext from "../../context/userContext.js";
 const Write = () =>{
-    var BlogData = {
-        BlogTitle : "",
-        BlogImage:"",
-        BlogContent:"",
-        PublishName : ""
-    }
+    const { user } = useContext(userStateContext)
+    const [BlogTitle, settitle] = useState('')
+    const [BlogImage, setImage] = useState('')
+    const [BlogContent, setcontent] = useState('')
     const SubmitBlog = (e) =>{
         e.preventDefault();
+        console.log()
+        if(BlogContent && BlogTitle && BlogImage){
+            var payload = { BlogTitle, BlogContent, BlogImage};
+            axios.post("/api/publishblog", payload, {headers : {
+                "auth-token" : user
+            }}).then((data) =>{
+                console.log(data)
+                alert(data.data.msg)
+            })
+            setImage('')
+            setcontent('')
+            settitle('')
+        }
     }
     return(
         <div className="writeBlog">
             <form action="" onSubmit={SubmitBlog} className="writeBlog-form">
                 <h2>Title </h2>
-                <input type="text" placeholder="Blog Title" required onChange={(e) =>{BlogData.BlogTitle = e.target.value}}/>
+                <input type="text" value={BlogTitle} placeholder="Blog Title" required onChange={(e) =>{settitle(e.target.value)}}/>
                 <h2>Image Link</h2>
-                <input type="text" placeholder="Enter Public Image Link (if any)" onChange={(e) => {BlogData.BlogImage = e.target.value}} />
+                <input type="text" value={BlogImage} placeholder="Enter Public Image Link (if any)" onChange={(e) => {setImage(e.target.value)}} />
                 <h2>Blog Content</h2>
-                <textarea name="" placeholder="Write Content" cols="30" rows="10" required onChange={(e) => {BlogData.BlogContent = e.target.value}}></textarea>
-                <h2>Publisher Name</h2>
-                <input type="text" placeholder="Enter Name" onChange={(e) => {BlogData.PublishName = e.target.value}} />
+                <textarea name="" value={BlogContent} placeholder="Write Content" cols="30" rows="10" required onChange={(e) => {setcontent(e.target.value)}}></textarea>
                 <br />
                 <button className="Sign-btn">Save</button>
                 </form>
