7735a38b9c805cd9ecf45b4ac18ab9eb591933f9
sachin320 <sachinvermavs786@gmail.com>
2021-12-17 23:42:12 +0530

    sign up, sign in components updated

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,56 +1,87 @@
-import React, { useContext, useState} from "react";
-import '../App.css';
+import React, { useContext, useState } from "react";
+import "../App.css";
 import { Link, useHistory } from "react-router-dom";
 import userStateContext from "../context/userContext.js";
 import axios from "axios";
-const Signin = () =>{
-    const history = useHistory()
-    const {setuser} = useContext(userStateContext)
-    const [luser, setlUser] = useState('');
-    const [password, setpassword] = useState('');  
-    const userSignin = async (e) =>{
-        e.preventDefault();
-        if(luser !== '' && password !== '' ){
-          let payload = { username : luser, password}
-          axios.post("/api/auth/signin", payload).then((data) =>{
-              localStorage.setItem("auth-token", data.data.authtoken)
-              setuser(data.data.authtoken);
-              alert(data.data.msg);
-              history.push('/');
-          })
-          setlUser('')
-          setpassword('')
+const Signin = () => {
+  const history = useHistory();
+  const { setuser } = useContext(userStateContext);
+  const [luser, setlUser] = useState("");
+  const [password, setpassword] = useState("");
+  const userSignin = async (e) => {
+    e.preventDefault();
+    if (luser !== "" && password !== "") {
+      let payload = { username: luser, password };
+      axios.post("/api/auth/signin", payload).then((data) => {
+        localStorage.setItem("auth-token", data.data.authtoken);
+        setuser(data.data.authtoken);
+        alert(data.data.msg);
+        if(data.data.success){
+            history.push("/");
         }
-        else{
-            setuser(null)
-            setlUser('')
-            setpassword('')
-            alert("Something Went Wrong :(")
-        }
+      });
+      setlUser("");
+      setpassword("");
+    } else {
+      setuser(null);
+      setlUser("");
+      setpassword("");
+      alert("Something Went Wrong :(");
     }
-    return(
-        <>
-        <div className="Signin">
-            <div className="Sign-in-form">
-                <form action="" onSubmit={userSignin} className="input-form">
-                    <h1>Sign In</h1>
-                    <hr style={{width:'90%', margin:'auto', marginTop:'20px', marginBottom:'50px'}} />
-                    <label htmlFor=""><i className="fa fa-user icon"> </i></label>
-                    <input type="text" autoComplete="username" value={luser} placeholder="Email" required onChange={(e) =>{setlUser(e.target.value)}} />
-                    <br />
-                    <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" autoComplete="current-password" value={password} placeholder="Password" onChange={(e) =>{setpassword(e.target.value)}} required />
-                    <br />
-                    <div className="form-link">
-                        <Link to="/signup"><p className="form-link2">Create new account</p></Link>
-                    </div>
-                    <button className="Sign-btn">Sign In</button>
-                </form>
+  };
+  return (
+    <>
+      <div className="Signin">
+        <div className="Sign-in-form">
+          <form action="" onSubmit={userSignin} className="input-form">
+            <h1>Sign In</h1>
+            <hr
+              style={{
+                width: "90%",
+                margin: "auto",
+                marginTop: "20px",
+                marginBottom: "50px",
+              }}
+            />
+            <label htmlFor="">
+              <i className="fa fa-user icon"> </i>
+            </label>
+            <input
+              type="text"
+              autoComplete="username"
+              value={luser}
+              placeholder="Email"
+              required
+              onChange={(e) => {
+                setlUser(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="">
+              <i className="fa fa-key icon"> </i>
+            </label>
+            <input
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+              required
+            />
+            <br />
+            <div className="form-link">
+              <Link to="/signup">
+                <p className="form-link2">Create new account</p>
+              </Link>
             </div>
-
+            <button className="Sign-btn">Sign In</button>
+          </form>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Signin;
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,62 +1,131 @@
-import React from "react";
-import '../App.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import "../App.css";
+import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
-const Signup = () =>{
-    var history = useHistory();
-    var FormData = {
-        username:"",
-        email:"",
-        password:"",
-        cpassword:""
-    } 
-    const Register = async (e) =>{
-        e.preventDefault();
-        if(FormData.cpassword === FormData.password){
-            console.log(FormData);
-            axios.post("/api/auth/signup", FormData).then((data)=>{
-                console.log(data);
-                alert(data.data.msg);
-                if(data.data.redirect){
-                    history.push("/signin")
-                }
-            }).catch((err)=>{
-                console.log(err);
-            })
-        }
-        else{
-            alert("Password & Confirm Password must be same")
-        }
+const Signup = () => {
+  var history = useHistory();
+  const [uname, setuname] = useState("");
+  const [email, setemail] = useState("");
+  const [pass, setpass] = useState("");
+  const [cpass, setcpass] = useState("");
+  const Register = async (e) => {
+    e.preventDefault();
+    if(uname.length < 4){
+      alert("Password should be at least 4 characters");
+      return;
     }
-    return(
-        <>
-        <div className="Signin">
-            <div className="Sign-in-form">
-                <form  onSubmit={Register} method="POST" className="input-form">
-                    <h1>Sign Up</h1>
-                    <hr style={{width:'90%', margin:'auto', marginTop:'20px', marginBottom:'50px'}} />
-                    <label htmlFor=""><i className="fa fa-user icon"> </i></label>
-                    <input type="text" placeholder="Username" required className="inputs" onChange={(e)=>{FormData.username = e.target.value;}} />
-                    <br />
-                    <label htmlFor=""><i className="fa fa-envelope icon"> </i></label>
-                    <input type="email" placeholder="Email" required className="inputs" onChange={(e)=>{FormData.email = e.target.value;}} />
-                    <br />
-                    <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" placeholder="Password" required className="inputs" onChange={(e)=>{FormData.password = e.target.value;}} />
-                    <br />
-                    <label htmlFor=""><i className="fa fa-key icon"> </i></label>
-                    <input type="password" placeholder="Confirm Password" required className="inputs" onChange={(e)=>{FormData.cpassword = e.target.value;}} />
-                    <br />
-                    <div className="form-link">
-                        <Link to="/signin"><p className="form-link2">Already have an account</p></Link>
-                    </div>
-                    <button className="Sign-btn">Sign Up</button>
-                </form>
+    if (pass.length < 6) {
+      alert("Password should be at least 6 characters");
+      return;
+    }
+    if (pass === cpass) {
+      var FormData = {
+        username: uname,
+        email,
+        password: pass,
+        cpassword: cpass,
+      };
+      console.log(FormData);
+      axios
+        .post("/api/auth/signup", FormData)
+        .then((data) => {
+          console.log(data);
+          alert(data.data.msg);
+          if (data.data.success) {
+            history.push("/signin");
+            setuname("");
+            setemail("");
+            setpass("");
+            setcpass("");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      alert("Password & Confirm Password must be same");
+    }
+  };
+  return (
+    <>
+      <div className="Signin">
+        <div className="Sign-in-form">
+          <form onSubmit={Register} method="POST" className="input-form">
+            <h1>Sign Up</h1>
+            <hr
+              style={{
+                width: "90%",
+                margin: "auto",
+                marginTop: "20px",
+                marginBottom: "50px",
+              }}
+            />
+            <label htmlFor="">
+              <i className="fa fa-user icon"> </i>
+            </label>
+            <input
+              type="text"
+              placeholder="Username"
+              required
+              className="inputs"
+              onChange={(e) => {
+                setuname(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="">
+              <i className="fa fa-envelope icon"> </i>
+            </label>
+            <input
+              type="email"
+              autoComplete="username"
+              placeholder="Email"
+              required
+              className="inputs"
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="">
+              <i className="fa fa-key icon"> </i>
+            </label>
+            <input
+              type="password"
+              autoComplete="new-password"
+              placeholder="Password"
+              required
+              className="inputs"
+              onChange={(e) => {
+                setpass(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="">
+              <i className="fa fa-key icon"> </i>
+            </label>
+            <input
+              type="password"
+              autoComplete="new-password"
+              placeholder="Confirm Password"
+              required
+              className="inputs"
+              onChange={(e) => {
+                setcpass(e.target.value);
+              }}
+            />
+            <br />
+            <div className="form-link">
+              <Link to="/signin">
+                <p className="form-link2">Already have an account</p>
+              </Link>
             </div>
-
+            <button className="Sign-btn">Sign Up</button>
+          </form>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Signup;
