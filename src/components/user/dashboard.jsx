2ed83dfa7ca13125370c90abebef4edff8d6ad2f
sachin320 <sachinvermavs786@gmail.com>
2021-12-17 18:15:19 +0530

    Dashboard Updated

--- src/components/user/dashboard.jsx
+++ src/components/user/dashboard.jsx
@@ -1,37 +1,67 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import '../../App.css';
 import { Link} from 'react-router-dom';
 import axios from 'axios';
+import userStateContext from '../../context/userContext';
 const Dashboard = () =>{
+    const { user } = useContext(userStateContext);
+    const [Blog, setBlog] = useState([{
+        BlogTitle : '',
+        BlogContent : '',
+        _id : ''
+    }])
+    useEffect(() =>{
+        async function fetchData(){
+        await axios.post("/api/getuserblog", {}, {
+            headers : {
+                "auth-token" : user
+            }
+        }).then((recvdata) =>{
+            setBlog(recvdata.data);
+        }).catch((err) =>{
+            console.log(err);
+        })
+    }
+    fetchData();
+    }, [user])
     return(
         <div className="DashBoard">
-            <div key="0">
-                <h1>No Published blogs </h1>
-                <Link to="/write">
-                    <button className="nav-btn">Publish First Blog </button>
-                </Link>
-            </div>
-            <>
+            {Blog.length === 0 ? (
+                <>
+                    <h1>No Published blogs </h1>
+                    <Link to="/write">
+                        <button className="nav-btn">Publish First Blog </button>
+                    </Link>
+                </>
+            )
+            : (
+                <>
             <h1>Your Publications </h1>
-                <div key="1" id="1" className="Blog-list">
-                    <div>
-                        <h1>title</h1>
-                    </div>
-                    <div>
-                        <Link to={`/readblog/`}><button className="nav-btn">Read</button></Link>
-                        <button onClick={() =>{
-                            axios.delete(`/api/deleteBlog/"1`).catch((err) =>{
-                                console.log("Error !")
-                            })
-                            alert("Blog deleted Successfully !")
-                            document.getElementById('1').style.display ="none";
+            {Blog.map((blog) =>{
+                return(
+                    <div key={blog._id} id={blog._id} className="Blog-list">
+                        <div>
+                            <h1>{blog.BlogTitle}</h1>
+                        </div>
+                        <div>
+                            <Link to={`/readblog/${blog._id}`}><button className="nav-btn">Read</button></Link>
+                            <button onClick={() =>{
+                                axios.delete(`/api/deleteBlog/${blog._id}`).catch((err) =>{
+                                    console.log("Error !")
+                                })
+                                alert("Blog deleted Successfully !")
+                                document.getElementById(blog._id).style.display ="none";
                             }} className="nav-btn">Delete</button>
+                        </div>
                     </div>
-                </div>
+                )
+            })}
             <Link to="/write">
                 <button className="nav-btn">Publish New Blog</button>
             </Link>
             </>
+            )}
+
         </div>
     )
 }
