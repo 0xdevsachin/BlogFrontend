2bb029b0b55cfa0e493756da6cc0f6d2703815eb
sachin320 <sachinvermavs786@gmail.com>
2021-12-18 00:50:58 +0530

    links updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,9 +20,14 @@ const contact = () =>{
                     <p> <i className="fas fa-code-branch"></i> VC : Github</p>
                 </div>
             </div>
-            <h1>Connect With Me 💌</h1>
-            <h2 style={{margin:"30px auto"}}>Sachin Verma</h2>
-            <h3><a href="http://github.com/0xdevsachin"><i className="fab fa-github-alt Footer-icons"></i></a><a href="http://linkedin.com/in/0xsachin"><i className="fab fa-linkedin Footer-icons"></i></a></h3>
+            <h1>Connect With Me</h1>
+            <h2 style={{margin:"30px auto"}}> <i className="fas fa-code"></i> Sachin Verma</h2>
+            <span>
+                <a href="http://github.com/0xdevsachin"><i className="fab fa-github-alt Footer-icons"></i></a>
+                <a href="http://linkedin.com/in/0xsachin" style={{margin : "auto 10px"}}><i className="fab fa-linkedin Footer-icons"></i></a>
+                <a href="mailto:0xsachinverma@gmail.com"><i className="fa fa-envelope Footer-icons"></i></a>
+                
+                </span>
         </div>
     )
 }
